694e286f08a39474b14e2b7ec06d009eaf2bc743
CarlosColnaghi <carlos.colnaghi@hotmail.com>
2025-09-22 20:46:58 -0300

    feat: add deadline field

--- app/src/main/java/com/example/mytodolist/view/TaskRegistrationFragment.kt
+++ app/src/main/java/com/example/mytodolist/view/TaskRegistrationFragment.kt
@@ -1,5 +1,7 @@
 package com.example.mytodolist.view
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,6 +11,7 @@ import android.widget.ArrayAdapter
 import com.example.mytodolist.R
 import com.example.mytodolist.data.State
 import com.example.mytodolist.databinding.FragmentTaskRegistrationBinding
+import java.util.Calendar
 
 class TaskRegistrationFragment : Fragment() {
 
@@ -32,6 +35,32 @@ class TaskRegistrationFragment : Fragment() {
         fragmentTaskRegistrationBinding = FragmentTaskRegistrationBinding.inflate(inflater, container, false).apply {
             stateSpinner.adapter = taskStateAdapter
             stateSpinner.setSelection(0)
+
+            deadlineEditText.setOnClickListener {
+                val calendar = Calendar.getInstance()
+
+                val datePicker = DatePickerDialog(
+                    requireContext(),
+                    { _, year, month, dayOfMonth ->
+                        val timePicker = TimePickerDialog(
+                            requireContext(),
+                            { _, hourOfDay, minute ->
+                                deadlineEditText.setText(
+                                    "%02d/%02d/%04d at %02d:%02d".format(dayOfMonth, month + 1, year, hourOfDay, minute)
+                                )
+                            },
+                            calendar.get(Calendar.HOUR_OF_DAY),
+                            calendar.get(Calendar.MINUTE),
+                            true
+                        )
+                        timePicker.show()
+                    },
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)
+                )
+                datePicker.show()
+            }
         }
         return fragmentTaskRegistrationBinding.root
     }
